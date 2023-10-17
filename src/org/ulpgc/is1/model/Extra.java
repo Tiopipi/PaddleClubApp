e19cdbba273807378ae32fe323f6a83ac551964a
Davidgarve <davidgaver2004@gmail.com>
2023-10-17 14:18:08 +0100

    Extra, Umpire and Equipment classes implemented

--- src/org/ulpgc/is1/model/Extra.java
+++ src/org/ulpgc/is1/model/Extra.java
@@ -1,4 +1,15 @@
 package org.ulpgc.is1.model;
 
 public abstract class Extra {
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public abstract String getName();
 }
--- src/org/ulpgc/is1/model/Umpire.java
+++ src/org/ulpgc/is1/model/Umpire.java
@@ -1,4 +1,29 @@
 package org.ulpgc.is1.model;
 
-public class Umpire {
+public class Umpire extends Extra{
+    private String name;
+    private String surname;
+
+    public Umpire(int price, String name, String surname) {
+        setPrice(price);
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String getName() {
+        return name + " " + surname;
+    }
 }
